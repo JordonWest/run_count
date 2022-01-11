6fc4584f1af664fb8b8e47ae437e5ccea9d895fa
Jordan West <jwest@drwholdings.com>
2022-01-11 10:45:17 -0600

    fiddling with styling. Convert yaml to json for ease of backup

--- data/squidbase.py
+++ data/squidbase.py
@@ -1,13 +1,15 @@
-import yaml, os, sys
+import yaml, os, sys, json
 
 
-file_path = f"{os.getcwd()}/data/dev.yaml" if os.environ['FLASK_ENV'] == 'development' else f"{os.getcwd()}/data/runs.yaml"
+file_path = f"{os.getcwd()}/data/dev.json" if os.environ['FLASK_ENV'] == 'development' else f"{os.getcwd()}/data/runs.json"
 
 def squidget():
-    yaml_file = open(file_path)
-    return (yaml.load(yaml_file, Loader=yaml.FullLoader))
+    json_file = open(file_path)
+    return json.load(json_file)
 
 def squidwrite(data):
-    yaml_file = open(file_path, "w")
-    yaml.dump(data, yaml_file, default_flow_style=False)
+#    yaml_file = open(file_path, "w")
+#    yaml.dump(data, yaml_file, default_flow_style=False)
+    with open(file_path, 'w') as f:
+        json.dump(data, f)
 
